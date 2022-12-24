5591a9ee8d739fd553f3dee1901e62910c23c56a
Zachary Whitelaw <zac.whitelaw@btinternet.com>
2022-12-24 12:49:58 +0000

    Update README.md and Rider refactor for Day1.cs

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -1,26 +1,20 @@
 namespace AdventOfCode2022.Day1;
 
 class Day1 : Problem {
-
 	public Day1(string input) : base(input) 
 	{
 	}
+	private static IEnumerable<int> ParseCalories(string raw)
+	{
+		string[] elves = raw.TrimEnd().Split(Environment.NewLine + Environment.NewLine);
+		return elves.Select(elf => elf.Split(Environment.NewLine).Select(int.Parse).Sum()).ToList();
+	}
     public override string SolvePart1()
     {
-        string[] elves = input.Split("\n\n");
-		IList<int> calories = new List<int>();
-		foreach (string elf in elves) {
-			calories.Add(elf.Split("\n").Select(x => int.Parse(x)).Sum());
-		}
-		return calories.OrderDescending().First().ToString();
+	    return ParseCalories(input).OrderDescending().First().ToString();
     }
     public override string SolvePart2()
     {
-        string[] elves = input.Split("\n\n");
-		IList<int> calories = new List<int>();
-		foreach (string elf in elves) {
-			calories.Add(elf.Split("\n").Select(x => int.Parse(x)).Sum());
-		}
-		return calories.OrderDescending().Take(3).Sum().ToString();
+	    return ParseCalories(input).OrderDescending().Take(3).Sum().ToString();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # AdventOfCode2022
 Problem solutions for advent of code 2022 (Mixture of Rust, C# and python)
+
+### Setup
+
+To download the problem input for a certain day, it is possible to delete the outstanding
+input.txt and run the python file:
+
+`python setup.py <day>`
+
+If day is left blank will retrieve input for current day.
+
+### Running
+
+Run an individual day with:
+
+`dotnet run <day>`
+
+Run all days with 
+
+`dotnet run all`
+
+Run the test input for a certain day with
+
+`dotnet run <day> test`
+
