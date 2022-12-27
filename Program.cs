83d3c0455462328605532653c082849a194b1a93
Zachary Whitelaw <zac.whitelaw@btinternet.com>
2022-12-27 12:51:51 +0000

    Add advent calendar for displaying problems to run

--- Program.cs
+++ Program.cs
@@ -2,29 +2,39 @@
 
 namespace AdventOfCode2022;
 
-class Program {
+class Program
+{
 
-    public static bool DISPLAY_OUTPUT = false;
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
         Type[] problemList = Assembly.GetExecutingAssembly().GetTypes()
             .Where(x => x.Name.StartsWith("Day"))
             .OrderBy(x => int.Parse(x.Name.Substring(3)))
             .ToArray();
 
-        if (args.Length < 1) 
+        if (args.Length < 1)
         {
+            ISet<int> doneDays = problemList.Select(x => int.Parse(x.Name.Substring(3))).Order().ToHashSet();
             Console.WriteLine("Please specify a day to run.");
             Console.WriteLine("Possible days:");
-            Console.WriteLine("{0}", string.Join(", ", problemList.Select(x => int.Parse(x.Name.Substring(3))).Order()));
+            for (int i = 0; i < 25; i++)
+            {
+                Console.Write(doneDays.Contains(i + 1) ? $"{(i + 1),2} " : "   ");
+                if ((i + 1) % 5 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            // Console.WriteLine("{0}", string.Join(", ", problemList.Select(x => int.Parse(x.Name.Substring(3))).Order()));
             return;
         }
 
         bool test = args.Length > 1 && (args[1] == "test" || args[1] == "t" || args[1] == "true");
         int day;
 
-        if (args[0].Equals("all")) 
+        if (args[0].Equals("all"))
         {
-            foreach (Type type in problemList) 
+            foreach (Type type in problemList)
             {
                 day = int.Parse(type.Name.Substring(3));
                 TryRunProblem(day, problemList, test);
@@ -34,15 +44,15 @@ class Program {
 
         day = int.Parse(args[0]);
 
-        string customInput = args.Length >= 2 && !test ? args[1] : null; 
+        string customInput = args.Length >= 2 && !test ? args[1] : null;
         TryRunProblem(day, problemList, test, customInput);
     }
 
-    private static void TryRunProblem(int day, Type[] problemList, bool test, string additionalInput = null) 
+    private static void TryRunProblem(int day, Type[] problemList, bool test, string additionalInput = null)
     {
         string inputPath = test ? $"Day{day}/test.txt" : $"Day{day}/input.txt";
 
-        if (!File.Exists(inputPath)) 
+        if (!File.Exists(inputPath))
         {
             Console.WriteLine($"Could not read \'{inputPath}\'.");
             return;
@@ -54,20 +64,31 @@ class Program {
         if (Activator.CreateInstance(type, input) is Problem problem)
         {
             PrintProblemOutput(problem);
-        } 
-        else 
+        }
+        else
         {
             Console.WriteLine($"Instance of {type.Name} could not be created.");
             return;
         }
     }
 
-    private static void PrintProblemOutput(Problem problem) {
+    private static void PrintProblemOutput(Problem problem)
+    {
         Console.WriteLine(problem.GetType().Name);
         string part1 = problem.SolvePart1();
         string part2 = problem.SolvePart2();
 
         Console.WriteLine($"Part 1: {part1}");
         Console.WriteLine($"Part 2: {part2}");
+    }
+
+    internal struct Options
+    {
+        public bool DisplayOutput = false;
+        public bool UsingTestInput = false;
+        public bool RunAllProblems = false;
+
+        public Options()
+        { }
     }
 }
